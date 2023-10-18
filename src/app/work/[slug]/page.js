861d2ae13ef482d7afcee6b1baf3dc00b0d9f6d4
Jordi Garreta <jordigarreta11@gmail.com>
2023-10-18 13:09:15 +0200

    button project link

--- src/app/work/[slug]/page.js
+++ src/app/work/[slug]/page.js
@@ -46,9 +46,11 @@ const ProjectPage = ({params}) => {
                         <div className={styles.project_info_right}>
                             <div className={styles.project_info_credits} dangerouslySetInnerHTML={{__html: project.acf.credits}}/>
                             {project.acf.project_link && (
-                                <button className={styles.project_link}>
-                                    <Link target="_blank" href={project.acf.project_link}>Project Link</Link>
-                                </button>
+                                <Link target="_blank" href={project.acf.project_link}>
+                                    <button className={styles.project_link}>
+                                        Project Link
+                                    </button>
+                                </Link>
                             )}
                         </div>
                     </div>
