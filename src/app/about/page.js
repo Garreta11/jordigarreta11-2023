3c8e53cdc65502f49d8a4af47271b1bceb0c93e4
Jordi Garreta <jordigarreta11@gmail.com>
2024-06-20 12:46:23 +0200

    new design work project

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,7 @@
-'use client'
-import styles from './About.module.scss'
-import { useEffect, useState, useRef, useInsertionEffect } from 'react'
-import { motion } from 'framer-motion'
+'use client';
+import styles from './About.module.scss';
+import { useEffect, useState, useRef, useInsertionEffect } from 'react';
+import { motion } from 'framer-motion';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Mousewheel, Autoplay, FreeMode } from 'swiper/modules';
@@ -12,118 +12,102 @@ const transitionFriends = { delay: 1.0, ease: [0.43, 0.13, 0.23, 0.96] };
 const transitionMarquee = { delay: 0.5, ease: [0.43, 0.13, 0.23, 0.96] };
 
 const AboutPage = () => {
+  const [aboutPage, setAbouPage] = useState();
+  const [cvPage, setCvPage] = useState();
 
-    const [aboutPage, setAbouPage] = useState()
-    const [cvPage, setCvPage] = useState()
+  // fetch data from wordpress
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(
+        'https://dashboard.jordigarreta.com/wp-json/wp/v2/pages'
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          data.forEach((page) => {
+            if (page.title.rendered === 'About me') {
+              setAbouPage(page);
+            }
+            if (page.title.rendered === 'cv') {
+              setCvPage(page);
+            }
+          });
+        });
+    }
+    fetchData();
+  }, []);
 
-    // fetch data from wordpress
-    useEffect(() => {
-        async function fetchData() {
-            const res = await fetch(
-                'https://dashboard.jordigarreta.com/wp-json/wp/v2/pages'
-            )
-                .then(res => res.json())
-                .then(data => {
-                    data.forEach(page => {
-                        if (page.title.rendered === 'About me') {
-                            setAbouPage(page)
-                        }
-                        if (page.title.rendered === 'cv') {
-                            setCvPage(page)
-                        }
-                    })
+  return (
+    <main className={styles.about}>
+      <div className={styles.about__wrapper}>
+        {aboutPage && (
+          <div className={styles.about_about}>
+            <motion.div
+              initial={{ opacity: 0, x: -20 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={transition}
+              className={styles.about_about_description}
+              dangerouslySetInnerHTML={{ __html: aboutPage.acf.description }}
+            />
+          </div>
+        )}
 
-                })
-        }
-        fetchData()
-    }, [])
-
-    return (
-        <main
-            className={styles.about}
-        >
-            <div className={styles.about__wrapper}>
-                {aboutPage && (
-                    <div className={styles.about_about}>
-                        <motion.div
-                            initial={{ opacity: 0, x: -20 }}
-                            animate={{ opacity: 1, x: 0 }}
-                            transition={transition}
-                            className={styles.about_about_description}
-                            dangerouslySetInnerHTML={{ __html: aboutPage.acf.description }} />
-                    </div>
-                )}
-
-                {aboutPage && (
-                    <Friends aboutPage={aboutPage} />
-                )}
-
-            </div>
-            {cvPage && (
-                <Marquee cvPage={cvPage} />
-            )}
-
-
-
-        </main>
-    )
-}
+        {aboutPage && <Friends aboutPage={aboutPage} />}
+      </div>
+      {cvPage && <Marquee cvPage={cvPage} />}
+    </main>
+  );
+};
 
 const Marquee = ({ cvPage }) => {
+  const [skills, setSkills] = useState([]);
 
-    const [skills, setSkills] = useState([])
+  useEffect(() => {
+    let s = cvPage.acf.skills.replaceAll('<p>', '');
+    s = s.replaceAll('</p>', '');
 
-    useEffect(() => {
-        let s = cvPage.acf.skills.replaceAll('<p>', '')
-        s = s.replaceAll('</p>', '')
+    setSkills(s.split('\n'));
+  }, []);
 
-        setSkills(s.split('\n'))
-
-    }, [])
-
-
-    return (
-        <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={transitionMarquee}
-            className={styles.about__marquee}
-        >
-            <div className={styles.about__marquee__content}>
-                <span className={styles.about__marquee__content__1}>
-                    {skills.map((skill, index) => (
-                        <span key={index} className={styles.about__marquee__content__skill}>
-                            {skill}
-                        </span>
-                    ))}
-                </span>
-                <span className={styles.about__marquee__content__2}>
-                    {skills.map((skill, index) => (
-                        <span key={index} className={styles.about__marquee__content__skill}>
-                            {skill}
-                        </span>
-                    ))}
-                </span>
-            </div>
-
-        </motion.div>
-    )
-}
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={transitionMarquee}
+      className={styles.about__marquee}
+    >
+      <div className={styles.about__marquee__content}>
+        <span className={styles.about__marquee__content__1}>
+          {skills.map((skill, index) => (
+            <span key={index} className={styles.about__marquee__content__skill}>
+              {skill}
+            </span>
+          ))}
+        </span>
+        <span className={styles.about__marquee__content__2}>
+          {skills.map((skill, index) => (
+            <span key={index} className={styles.about__marquee__content__skill}>
+              {skill}
+            </span>
+          ))}
+        </span>
+      </div>
+    </motion.div>
+  );
+};
 
 const Friends = ({ aboutPage }) => {
+  return (
+    <motion.div
+      className={styles.about__friends}
+      initial={{ opacity: 0, x: 20 }}
+      animate={{ opacity: 1, x: 0 }}
+      transition={transitionFriends}
+    >
+      <h3>FRIENDS</h3>
 
-    return (
-        <motion.div
-            className={styles.about__friends}
-            initial={{ opacity: 0, x: 20 }}
-            animate={{ opacity: 1, x: 0 }}
-            transition={transitionFriends}
-        >
-            <h3>FRIENDS</h3>
-
-            <div dangerouslySetInnerHTML={{ __html: aboutPage.acf.friends }} />
-        </motion.div>
-    )
-}
+      <div dangerouslySetInnerHTML={{ __html: aboutPage.acf.friends }} />
+    </motion.div>
+  );
+};
 
 export default AboutPage;
--- src/app/work/[slug]/page.js
+++ src/app/work/[slug]/page.js
@@ -1,98 +1,138 @@
-'use client'
-import Link from 'next/link'
-import styles from './project.module.scss'
-import { useEffect, useState } from "react";
+'use client';
+import Link from 'next/link';
+import styles from './project.module.scss';
+import { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 
-const transitionTitle = { duration: 1, duration: 1, ease: [0.43, 0.13, 0.23, 0.96] };
-const transitionDescription = { delay: 0.5, duration: 1, ease: [0.43, 0.13, 0.23, 0.96] };
-const transitionCredits = { delay: 1, duration: 1, ease: [0.43, 0.13, 0.23, 0.96] };
+const transitionTitle = {
+  duration: 1,
+  duration: 1,
+  ease: [0.43, 0.13, 0.23, 0.96],
+};
+const transitionDescription = {
+  delay: 0.5,
+  duration: 1,
+  ease: [0.43, 0.13, 0.23, 0.96],
+};
+const transitionCredits = {
+  delay: 1,
+  duration: 1,
+  ease: [0.43, 0.13, 0.23, 0.96],
+};
 
-const ProjectPage = ({params}) => {
-    const [project, setProject] = useState()
-    const [projectTitle, setProjectTitle] = useState()
-    const [nextProject, setNextProject] = useState()
+const ProjectPage = ({ params }) => {
+  const [project, setProject] = useState();
+  const [projectTitle, setProjectTitle] = useState();
+  const [nextProject, setNextProject] = useState();
+  const [prevProject, setPrevProject] = useState();
 
-    useEffect(() => {
-        async function fetchData() {
-            const res = await fetch(
-                'https://dashboard.jordigarreta.com/wp-json/wp/v2/posts'
-            )
-            .then(res => res.json())
-            .then(data => {
-                data.forEach(d => {
-                    if (d.slug === params.slug) {
-                        setProject(d)
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(
+        'https://dashboard.jordigarreta.com/wp-json/wp/v2/posts'
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          data.forEach((d) => {
+            if (d.slug === params.slug) {
+              setProject(d);
 
-                        const parser = new DOMParser();
-                        const parsedEntity = parser.parseFromString(d.title.rendered, "text/html");
-                        const t = parsedEntity.documentElement.textContent;
+              const parser = new DOMParser();
+              const parsedEntity = parser.parseFromString(
+                d.title.rendered,
+                'text/html'
+              );
+              const t = parsedEntity.documentElement.textContent;
 
-                        setProjectTitle(t)
+              setProjectTitle(t);
 
-                        if (d.next !== null) {
-                            setNextProject(d.next.slug)
-                        } else {
-                            setNextProject('oggetti-fanno-musica')
-                        }
-                    }
-                })
-            })
-        }
-        fetchData()
-    }, [])
+              console.log(d);
 
+              if (d.next !== null) {
+                setNextProject(d.next.slug);
+              } else {
+                setNextProject('oggetti-fanno-musica');
+              }
+              if (d.previous !== null) {
+                setPrevProject(d.previous.slug);
+              } else {
+                setPrevProject('starmax');
+              }
+            }
+          });
+        });
+    }
+    fetchData();
+  }, []);
 
-    return(
-        <section className={styles.project}>
-            {project && (
-                <>
-                    <motion.h1
-                        className={styles.project_title}
-                        initial={{opacity: 0, x: -20}}
-                        animate={{opacity: 1, x: 0}}
-                        transition={transitionTitle}
-                    >
-                        {projectTitle}
-                    </motion.h1>
-                    
-                    <div className={styles.project_info}>
-                        <motion.div
-                            className={styles.project_info_description}
-                            initial={{opacity: 0, x: -20}}
-                            animate={{opacity: 1, x: 0}}
-                            transition={transitionDescription}
-                        >
-                            <p>{project.acf.description}</p>
-                            <div className={styles.project_images} dangerouslySetInnerHTML={{__html: project.content.rendered}}/>
-                        </motion.div>
-                        
-                        <motion.div
-                            className={styles.project_info_right}
-                            initial={{opacity: 0, x: 20}}
-                            animate={{opacity: 1, x: 0}}
-                            transition={transitionCredits}
-                        >
-                            <div className={styles.project_info_credits} dangerouslySetInnerHTML={{__html: project.acf.credits}}/>
-                            {project.acf.project_link && (
-                                <Link target="_blank" href={project.acf.project_link} className={styles.project_link}>
-                                    Visit project
-                                </Link>
-                            )}
-                        </motion.div>
-                    </div>
+  return (
+    <section className={styles.project}>
+      {project && (
+        <>
+          <motion.h1
+            className={styles.project_title}
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={transitionTitle}
+          >
+            {projectTitle}
+          </motion.h1>
 
-                    <div className={styles.project_next}>
-                        <Link className={styles.project_next_link} href={`/work/${nextProject}`}>
-                            Next Project
-                        </Link>
-                    </div>
-                </>
-            )}
-            
-        </section>
-    )
+          <div className={styles.project_info}>
+            <motion.div
+              className={styles.project_info_description}
+              initial={{ opacity: 0, x: -20 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={transitionDescription}
+            >
+              <p>{project.acf.description}</p>
+              <div
+                className={styles.project_images}
+                dangerouslySetInnerHTML={{ __html: project.content.rendered }}
+              />
+            </motion.div>
 
-}
+            <motion.div
+              className={styles.project_info_right}
+              initial={{ opacity: 0, x: 20 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={transitionCredits}
+            >
+              <h2 style={{ marginBottom: '20px' }}>CREDITS</h2>
+              <div
+                className={styles.project_info_credits}
+                dangerouslySetInnerHTML={{ __html: project.acf.credits }}
+              />
+              {project.acf.project_link && (
+                <Link
+                  target='_blank'
+                  href={project.acf.project_link}
+                  className={styles.project_link}
+                >
+                  Visit project
+                </Link>
+              )}
+            </motion.div>
+          </div>
+
+          <div className={styles.project_next}>
+            <Link
+              className={styles.project_next_link}
+              href={`/work/${prevProject}`}
+            >
+              Prev Project
+            </Link>
+            <Link
+              className={styles.project_next_link}
+              href={`/work/${nextProject}`}
+            >
+              Next Project
+            </Link>
+          </div>
+        </>
+      )}
+    </section>
+  );
+};
 
 export default ProjectPage;
